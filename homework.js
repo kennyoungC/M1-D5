ba46e56ef85bd4cfb465348c06ecdb3ce3a0e13a
kennyoungC <96569081+kennyoungC@users.noreply.github.com>
2022-02-18 22:56:01 +0200

    exercise 5 and 6 done

--- homework.js
+++ homework.js
@@ -93,13 +93,26 @@ Write a function "strivify" which accepts a string.
 It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
 */
 
-/* WRITE YOUR CODE HERE */
-writeLine("EXERCISE 6");
-/*
+// let strivify = function (inString) {
+//   if (inString === "Strive") {
+//     console.log(inString);
+//   } else {
+//     let striveFront = "Strive";
+//     console.log(striveFront.concat(" ", inString));
+//   }
+// };
+// strivify("strive house");
+// writeLine("EXERCISE 6");
+// /*
 Write a function "check3and7" which accepts a positive number and check if it is a multiple of 3 or a multiple of 7.
 HINT: Module Operator
 */
-
+let check3and7 = function (positivenumber) {
+  if (positivenumber % 3 === 0 || positivenumber % 7 === 0)
+    console.log(positivenumber);
+  else console.log(`you can only input a multiply of 5 or 3 here, thank you`);
+};
+check3and7(4);
 /* WRITE YOUR CODE HERE */
 writeLine("EXERCISE 7");
 /*
