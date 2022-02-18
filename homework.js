df4c4e8126ec025baf5547b624858c5a1ae856f1
kennyoungC <96569081+kennyoungC@users.noreply.github.com>
2022-02-18 19:15:04 +0200

    exercise number 1 & 2 solved

--- homework.js
+++ homework.js
@@ -9,58 +9,109 @@ let writeLine = function (title) {
   console.log(`\n>-----------------< ${title} >-----------------<`);
 };
 writeLine("EXERCISE 1");
-/* EXERCISE 1
+/* 
 Write a function "area" which receives 2 parameters (l1,l2) and calculates the area of the associated rectangle.
 */
 
-/* EXERCISE 2
+let area = function (l1, l2) {
+  let result = l1 * l2;
+  return result;
+};
+let length = 4;
+let width = 7;
+let myans = area(length, width);
+
+writeLine("EXERCISE 2");
+/*
 Write a function "crazySum" which receives two integers. It should return the sum of those two values, but if the two values are the same then it should return their sum multiplied by 3.
 */
 
-/* EXERCISE 3
+let crazySum = function (int1, int2) {
+  if (int1 === int2) {
+    let multBy3 = (int1 + int2) * 3;
+    console.log(multBy3);
+  } else {
+    let justSum = int1 + int2;
+    console.log(justSum);
+  }
+};
+
+let firstNumber = 10;
+let secondNumber = 6;
+crazySum(firstNumber, secondNumber);
+
+// const crazySum = function (num1, num2) {
+//   intNum1 = parseInt(num1);
+//   intNum2 = parseInt(num2);
+
+//   if (intNum1 === intNum2) {
+//     let multBy3 = (intNum1 + intNum2) * 3;
+//     console.log(multBy3);
+//   } else {
+//     let justSum = intNum2 + intNum2;
+//     console.log(justSum);
+//   }
+// };
+// crazySum(2, 2);
+
+writeLine("EXERCISE 3");
+/* 
 Write a function "crazyDiff" that computes the absolute difference between a given number and 19. 
 It should return triple their absolute difference if the given number is greater than 19.
 */
 
-/* EXERCISE 4
+let crazyDiff = function (givenNumber, constant) {
+  if (givenNumber > 19 && constant === 19) {
+    let tripleReturnDifference = 3 * (givenNumber - 19);
+    console.log(tripleReturnDifference);
+  } else {
+    let absoluteDifference = givenNumber - constant;
+    console.log(absoluteDifference);
+  }
+};
+crazyDiff(26, 19);
+
+writeLine("EXERCISE 4");
+/*
 Write a function "boundary" which accept an integer n and returns true if n is within 20 and 100 (included) or if it's equal to 400.
 */
 
 /* WRITE YOUR CODE HERE */
 
-/* EXERCISE 5
+writeLine("EXERCISE 5");
+/* 
 Write a function "strivify" which accepts a string.
 It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
 */
 
 /* WRITE YOUR CODE HERE */
-
-/* EXERCISE 6
+writeLine("EXERCISE 6");
+/*
 Write a function "check3and7" which accepts a positive number and check if it is a multiple of 3 or a multiple of 7.
 HINT: Module Operator
 */
 
 /* WRITE YOUR CODE HERE */
-
-/* EXERCISE 7
+writeLine("EXERCISE 7");
+/*
 Write a function "reverseString" to programmatically reverse a given string (es.: Strive => evirtS).
 */
 
 /* WRITE YOUR CODE HERE */
-
-/* EXERCISE 8
+writeLine("EXERCISE 8");
+/* 
 Write a function "upperFirst" to capitalize the first letter of each word of a given string passed as a parameter.
 */
 
 /* WRITE YOUR CODE HERE */
-
-/* EXERCISE 9
+writeLine("EXERCISE 9");
+/*
 Write a function "cutString" to create a new string without the first and last character of a given string.
 */
 
 /* WRITE YOUR CODE HERE */
-
-/* EXERCISE 10
+writeLine("EXERCISE 10");
+/*
 Write a function "giveMeRandom" which accepts a number n and returns an array containing n random numbers between 0 and 10.
 */
 
